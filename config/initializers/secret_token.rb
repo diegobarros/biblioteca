88a839b8c97ca17ad8391de44520ee00978c2d65
Diego Augusto de Faria Barros <diego@diego-IPMH61R2.(none)>
2012-10-17 07:48:49 -0300

    configuracao do banco de dados

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblioteca::Application.config.secret_token = 'c36f7df924c7f88151871609240d25b507f137fb07b28aa978357e1a1965925454f5532e0438cf89a02d2cd761e98f08cb1f30de20274380727863c57296fea5'
